2394bb66ee7e8e492641c6a536edaee3a8de3859
brunolucero19 <brunolucero2419@gmail.com>
2024-11-11 15:05:34 -0300

    TP de React hasta video 7 completado.

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { CounterComponent } from './components/counterComponent/CounterComponent'
+import { FormComponent } from './components/formComponent/FormComponent'
+import { MiPrimerComponente } from './components/miPrimerComponente/MiPrimerComponente'
+import { UseEffectComponent } from './components/useEffectComponent/UseEffectComponent'
+
+export const App = () => {
+  //   const [enableCounter, setEnableCounter] = useState<boolean>(false)
+  //   const textButton = enableCounter ? 'Ocultar' : 'Mostrar'
+
+  return (
+    <>
+      <MiPrimerComponente text='Aguante Boca' color='blue' fontSize={32} />
+      {/* {enableCounter && <CounterComponent />}
+      <button
+        onClick={() => {
+          setEnableCounter(!enableCounter)
+        }}
+      >
+        {textButton} counter
+      </button> */}
+      <CounterComponent />
+      <UseEffectComponent />
+      <FormComponent />
+    </>
+  )
+}
